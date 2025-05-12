80f466b67202031670d283844a873d8b7e3d0838
Shivam Kaushik <shivamkaushik1771@gmail.com>
2025-05-12 21:29:42 +0530

    Create app.js

---
+++ frontend/src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Signup from './components/Signup';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Solo Leveling Tracker</h1>
+            <Signup />
+        </div>
+    );
+}
+
+export default App;
